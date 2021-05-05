99601a81125ac267947f7f8b86aa24f7e3e1e3a9
chriamue <chriamue@users.noreply.github.com>
2021-05-05 05:25:15 +0000

    deploy: d51a07b758bdfcd513409da0fe2a74a4ad7a6f25

--- watermark/remoteEntry.js
+++ watermark/remoteEntry.js
@@ -796,7 +796,7 @@ __webpack_require__.d(exports, {
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"333":{"641":"1ce29697c38572e7d861"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"333":{"641":"b164a3f0cc6e411a0239"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
